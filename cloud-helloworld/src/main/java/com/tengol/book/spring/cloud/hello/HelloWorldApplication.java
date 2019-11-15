d20a21bcfb1c85639bfe1e9e2d5c550051c7bb90
dongrui <dongrui@aerozhonghuan.com>
2019-11-15 18:19:51 +0800

    hello world & predicate

---
+++ cloud-helloworld/src/main/java/com/tengol/book/spring/cloud/hello/HelloWorldApplication.java
@@ -0,0 +1,20 @@
+package com.tengol.book.spring.cloud.hello;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * HelloWorldApplication
+ *
+ * @author dongrui
+ * @date 2019/11/15 17:34
+ */
+@Slf4j
+@SpringBootApplication
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+        log.info("start successfully ...");
+    }
+}
---
+++ cloud-helloworld/src/main/java/com/tengol/book/spring/cloud/hello/controller/CookieController.java
@@ -0,0 +1,30 @@
+package com.tengol.book.spring.cloud.hello.controller;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * CookieController
+ *
+ * @author dongrui
+ * @date 2019/11/15 17:36
+ */
+@RestController
+public class CookieController {
+    @GetMapping("/test/cookie")
+    public ResponseEntity<String> testCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        return ResponseEntity.ok(JSON.toJSONString(cookies));
+    }
+
+    @GetMapping("/test/header")
+    public ResponseEntity<String> testHeader(HttpServletRequest request) {
+        String header = request.getHeader("x-app-code");
+        return ResponseEntity.ok(header);
+    }
+}
